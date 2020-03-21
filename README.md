a40a0296c32de9c26a49e7042c2c4c3b1e164de8
Pavel Kral <pavel@pavelkral.eu>
2020-03-21 14:41:27 +0100

    Implement /api/v1/start endpoint

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # COVID Mock API
 
-TBC
+
+## /api/v1/start
+
+curl --header "Content-Type: application/json" --request POST --data '{"login": "aaa","password": "bbb","location": "123"}' http://localhost:8080/api/v1/start
--- api/app.py
+++ api/app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def root():
-    return "Hoooiiii"
+    return "Hello"
+
+
+@app.route("/api/v1/start", methods=["POST"])
+def login_start():
+    success_response = {
+        "result": "success",
+        "token": "djE6MTU4NDcxOTcxNzoxMjM0NTpoYXNoCg==",
+    }
+    error_response = {
+        "result": "error",
+        "code": "invalid-credentials",
+        "message": "Přihlášení se nezdařilo, nezadali jste chybné heslo?",
+    }
+    error_response_location = {
+        "result": "error",
+        "code": "invalid-location",
+        "message": "Taková lokalita neexistuje. Kontaktujte koordinátora.",
+    }
+
+    return jsonify(success_response)
 
 
 if __name__ == "__main__":
